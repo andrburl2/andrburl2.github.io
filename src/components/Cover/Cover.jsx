5f9e6c1a01fbece8c7e6f23b407a86b0806c1823
Andrey Burlakov <andrburl2@yandex.ru>
2022-02-16 11:54:45 +0300

    Обновил стили компонентов Cover, Footer. Исправил мелкие ошибки и улучшил адаптацию на маленьких разрешениях

--- src/components/Cover/Cover.jsx
+++ src/components/Cover/Cover.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import './cover.scss';
 
-import coverBg from '../../images/cover-bg.jpg';
-
 function Cover() {
   return (
     <section className='cover'>
-      <a className='cover__link' href='https://stampsy.com/na-elektrichkakh-do-baikala' target='_blank' rel='noreferrer'>
+      <a className='cover__link' href='https://stampsy.com/na-elektrichkakh-do-baikala' target='_blank' rel='noreferrer' title='На электричках до Байкала'>
         <h2 className='cover__title'>До Байкала «на собаках»</h2>
         <p className='cover__subtitle'>По мотивам учебной темы о Транссибе и iframes — путешествие от столицы до Байкала на электричках.</p>
-
-        <img className='cover__bg' src={coverBg} alt='Стоянка поездов' />
       </a>
     </section>
   )
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,16 +3,30 @@ import './footer.scss';
 
 function Footer() {
   return (
-    <footer className='footer'>
+    <footer className='footer section'>
       <ul className='footer__links'>
-      <li><a className='footer__link' href='https://yandex.ru/maps' target='_blank' rel='noreferrer'>Карты</a></li>
-      <li><a className='footer__link' href='https://yandex.ru/pogoda' target='_blank' rel='noreferrer'>Погода</a></li>
-      <li><a className='footer__link' href='https://rasp.yandex.ru' target='_blank' rel='noreferrer'>Расписание</a></li>
-      <li><a className='footer__link' href='https://calendar.yandex.ru' target='_blank' rel='noreferrer'>Календарь</a></li>
-      <li><a className='footer__link' href='https://travel.yandex.ru' target='_blank' rel='noreferrer'>Путешествия</a></li>
+        <li className='footer__link-item'>
+          <a className='footer__link' href='https://yandex.ru/maps' target='_blank' rel='noreferrer' title='Яндекс карты'>Карты</a>
+        </li>
+
+        <li className='footer__link-item'>
+          <a className='footer__link' href='https://yandex.ru/pogoda' target='_blank' rel='noreferrer' title='Яндекс погода'>Погода</a>
+        </li>
+
+        <li className='footer__link-item'>
+          <a className='footer__link' href='https://rasp.yandex.ru' target='_blank' rel='noreferrer' title='Яндекс Расписания'>Расписание</a>
+        </li>
+
+        <li className='footer__link-item'>
+          <a className='footer__link' href='https://calendar.yandex.ru' target='_blank' rel='noreferrer' title='Яндекс Календарь'>Календарь</a>
+        </li>
+
+        <li className='footer__link-item'>
+          <a className='footer__link' href='https://travel.yandex.ru' target='_blank' rel='noreferrer' title='Яндекс Путешествия'>Путешествия</a>
+        </li>
       </ul>
       
-      <p className='footer__copyright'>&copy; 2019. Бурлаков Андрей</p>
+      <p className='footer__copyright'>&copy; 2022. Бурлаков Андрей</p>
     </footer>
   )
 }
